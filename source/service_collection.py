47a3fbfd0fc743d2367788ef4b7fade9b4a6acaf
https-richardy <code.richardy@gmail.com>
2024-08-04 17:17:54 -0300

    new services registered in the services collection: GitDiffManager and CommitAssistant

--- source/service_collection.py
+++ source/service_collection.py
@@ -5,10 +5,20 @@ from dependency_injector import containers, providers
 
 from .configuration import Configuration
 from .services.gemini_service import GeminiService
+from .services.commit_assistant import CommitAssistant
+from .services.git_diff_manager import GitDiffManager
 
 class ServiceCollection(containers.DeclarativeContainer):
     configuration = providers.Singleton(Configuration)
+    diff_manager = providers.Singleton(GitDiffManager)
+
     gemini_service = providers.Factory(
         GeminiService,
         configuration=configuration
+    )
+
+    commit_assistant = providers.Factory(
+        CommitAssistant,
+        diff_manager=diff_manager,
+        gemini_service=gemini_service
     )
