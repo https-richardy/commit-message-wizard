0cb40c9d66a06e8b4630e1ed1fad1e27fa0111a1
https-richardy <code.richardy@gmail.com>
2024-08-04 17:19:01 -0300

    added the "CommitAssistant" service to the public interface of the services module.

--- source/services/__init__.py
+++ source/services/__init__.py
@@ -5,8 +5,10 @@ from typing import Sequence
 
 from .gemini_service import GeminiService
 from .git_diff_manager import GitDiffManager
+from .commit_assistant import CommitAssistant
 
 __all__: Sequence[str] = [
     "GeminiService",
     "GitDiffManager",
+    "CommitAssistant",
 ]
