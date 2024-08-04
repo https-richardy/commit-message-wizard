ae769b117db744235ab901358d3ab6775ded55a5
https-richardy <code.richardy@gmail.com>
2024-08-04 17:00:59 -0300

    added the GitDiffManager service, thus defining the public API of the "services" module.

--- source/services/__init__.py
+++ source/services/__init__.py
@@ -4,7 +4,9 @@
 from typing import Sequence
 
 from .gemini_service import GeminiService
+from .git_diff_manager import GitDiffManager
 
 __all__: Sequence[str] = [
     "GeminiService",
+    "GitDiffManager",
 ]
