5d8b399905efefe7aefb15d7d40cc53d7b219859
https-richardy <code.richardy@gmail.com>
2024-08-04 17:22:33 -0300

    added batch classes defining the public interface of the "source" module

--- source/__init__.py
+++ source/__init__.py
@@ -1,2 +1,16 @@
 # author: Richard Garcia (https://github.com/https-richardy)
 # license: MIT
+
+from typing import Sequence
+
+from . import services
+
+from .service_collection import ServiceCollection
+from .configuration import Configuration
+
+
+__all__: Sequence[str] = [
+    "services",
+    "ServiceCollection",
+    "Configuration",
+]
