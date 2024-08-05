24e2a657a71303a64068d368767026c146c03df5
https-richardy <code.richardy@gmail.com>
2024-08-04 22:14:56 -0300

    DiffOptions added to the source module's public API.

--- source/__init__.py
+++ source/__init__.py
@@ -7,10 +7,12 @@ from . import services
 
 from .service_collection import ServiceCollection
 from .configuration import Configuration
+from .diff_options import DiffOptions
 
 
 __all__: Sequence[str] = [
     "services",
     "ServiceCollection",
     "Configuration",
+    "DiffOptions"
 ]
