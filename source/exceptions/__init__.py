ff75f3907d039f27714bba6f0a6d1b7eca1f3c05
https-richardy <code.richardy@gmail.com>
2024-08-04 22:51:13 -0300

    added "NoChangesDetectedException" to public API do module "exceptions".

---
+++ source/exceptions/__init__.py
@@ -0,0 +1,9 @@
+# author: Richard Garcia (https://github.com/https-richardy)
+# license: MIT
+
+from typing import Sequence
+from .no_changes_detected_exception import NoChangesDetectedException
+
+__all__: Sequence[str] = [
+    "NoChangesDetectedException",
+]
