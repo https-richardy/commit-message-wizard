c3e8f83cb30bd81bdda891354862b78fd04d99ee
https-richardy <code.richardy@gmail.com>
2024-08-04 22:38:37 -0300

    argument parser registered in the services collection.

--- source/service_collection.py
+++ source/service_collection.py
@@ -4,6 +4,7 @@
 from dependency_injector import containers, providers
 
 from .configuration import Configuration
+from .parse_arguments import DiffOptionsParser
 from .services.gemini_service import GeminiService
 from .services.commit_assistant import CommitAssistant
 from .services.git_diff_manager import GitDiffManager
@@ -11,6 +12,7 @@ from .services.git_diff_manager import GitDiffManager
 class ServiceCollection(containers.DeclarativeContainer):
     configuration = providers.Singleton(Configuration)
     diff_manager = providers.Singleton(GitDiffManager)
+    arguments_parser = providers.Factory(DiffOptionsParser)
 
     gemini_service = providers.Factory(
         GeminiService,
