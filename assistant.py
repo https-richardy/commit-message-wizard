55d2eedccd910025fa69f569b4b5822676601b5e
https-richardy <code.richardy@gmail.com>
2024-08-04 22:39:38 -0300

    adds support for command line arguments

--- assistant.py
+++ assistant.py
@@ -1,12 +1,16 @@
 # author: Richard Garcia (https://github.com/https-richardy)
 # license: MIT
 
-from source import ServiceCollection
+from source import ServiceCollection, DiffOptionsParser
 from source.services import CommitAssistant
 
 services = ServiceCollection()
+
 assistant: CommitAssistant = services.commit_assistant()
+arguments_parser: DiffOptionsParser = services.arguments_parser()
 
 if __name__ == "__main__":
-    commit_message = assistant.generate_commit_message()
+    options = arguments_parser.parse_arguments()
+    commit_message = assistant.generate_commit_message(options)
+
     print(commit_message)
